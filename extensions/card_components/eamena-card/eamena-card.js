35060608b57c76bc53b988945679191e7bfdc1f8
Ash Smith <ash.smith@soton.ac.uk>
2020-03-12 17:44:26 +0000

    Replaced card back into original folder, hopefully this will work!

--- extensions/card_components/eamena-card/eamena-card.js
+++ extensions/card_components/eamena-card/eamena-card.js
@@ -5,7 +5,7 @@ define([
     return ko.components.register('eamena-card', {
         viewModel: CardComponentViewModel,
         template: {
-            require: 'text!templates/views/components/cards/eamena-card.htm'
+            require: 'text!templates/views/components/card_components/eamena-card.htm'
         }
     });
 });
